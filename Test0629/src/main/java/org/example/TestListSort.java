031e6e06bb83d162cb69d5868ca57757491e6674
HuangTao <675919853@qq.com>
2021-06-29 14:19:06 +0800

    Test ListSort

--- Test0629/src/main/java/org/example/TestListSort.java
+++ Test0629/src/main/java/org/example/TestListSort.java
@@ -1,4 +1,32 @@
 package org.example;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class TestListSort {
+
+    @Test
+    public void listSort(){
+
+        List<String> list = new ArrayList<>();
+        list.add("52ss");
+        list.add("zzz.hk.cn");
+        list.add("www.baidu.com");
+        list.add("www.google.com");
+        list.add("gf.cn");
+
+        for (String str:list) {
+            System.out.println(str);
+        }
+
+        System.out.println("*****************************after**********************************");
+
+        list.sort(String::compareTo);
+
+        for (String str:list) {
+            System.out.println(str);
+        }
+    }
 }
