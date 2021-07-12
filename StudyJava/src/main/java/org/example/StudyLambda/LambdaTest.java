9c3c9fc0c42860d1fd080c1eac2bd3d5959d075a
HuangTao <675919853@qq.com>
2021-07-12 17:35:39 +0800

    lambda表达式的学习

--- StudyJava/src/main/java/org/example/StudyLambda/LambdaTest.java
+++ StudyJava/src/main/java/org/example/StudyLambda/LambdaTest.java
@@ -2,6 +2,8 @@ package org.example.StudyLambda;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Comparator;
+
 /**
  * @Author HuangTao
  * @Description //TODO 测试Lambda表达式
@@ -24,6 +26,28 @@ public class LambdaTest {
 
         Runnable r2 =() -> System.out.println("这个就是Lambda表达式,真牛逼！");
         r2.run();
+    }
+
+    @Test
+    public void Test2(){
+        Comparator<Integer> com1 = new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return Integer.compare(o1,o2);
+            }
+        };
+
+        Integer x = 12;
+        int compare1 = com1.compare(12,13);
+        System.out.println(compare1);
+        System.out.println("****************************************");
+
+        Comparator<Integer> com2 = (o1,o2) -> Integer.compare(o1,o2);
+
+        int compare2 = com2.compare(13,12);
+        System.out.println(compare2);
 
     }
+
+
 }
